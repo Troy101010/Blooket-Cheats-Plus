6a16f1065b7721f0eb2d8d4369c2c492fe22bb36
DannyDan0167 <162914732+DannyDan0167@users.noreply.github.com>
2024-05-11 15:16:00 -0400

    Update GUI.meta.js

--- Update/GUI.meta.js
+++ Update/GUI.meta.js
@@ -1,1 +1,29 @@
-
+// ==UserScript==
+// @name         Blooket Cheats Plus
+// @namespace    https://github.com/DannyDan0167/Blooket-Cheats
+// @version      13.0
+// @description  Blooket Cheats Plus
+// @downloadURL  https://github.com/DannyDan0167/Blooket-Cheats-Plus/raw/main/GUI/Gui.user.js
+// @author       DannyDan0167
+// @match        https://dashboard.blooket.com/*
+// @match        https://play.blooket.com/*
+// @match        https://towerdefense2.blooket.com/*
+// @match        https://monsterbrawl.blooket.com/*
+// @match        https://towerdefense.blooket.com/*
+// @match        https://cafe.blooket.com/*
+// @match        https://factory.blooket.com/*
+// @match        https://crazykingdom.blooket.com/*
+// @match        https://towerofdoom.blooket.com/*
+// @match        https://goldquest.blooket.com/*
+// @match        https://cryptohack.blooket.com/*
+// @match        https://fishingfrenzy.blooket.com/*
+// @match        https://deceptivedinos.blooket.com/*
+// @match        https://battleroyale.blooket.com/*
+// @match        https://racing.blooket.com/*
+// @match        https://blookrush.blooket.com/*
+// @match        https://classic.blooket.com/*
+// @match        https://pirate.blooket.com/*
+// @icon         https://i.ibb.co/sWqBm0K/1024.png
+// @grant        none
+// @require     https://unpkg.com/idb-keyval@6.0.3/dist/umd.js
+// ==/UserScript==
