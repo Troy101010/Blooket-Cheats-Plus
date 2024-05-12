e8991c7726e1bded75880c7f8b2055835f28b7b7
DannyDan0167 <162914732+DannyDan0167@users.noreply.github.com>
2024-05-12 16:01:22 -0400

    Update Gui.user.js

--- GUI/Gui.user.js
+++ GUI/Gui.user.js
@@ -861,7 +861,7 @@
                     Comic: "comic",
                     Crayon: "crayon",
                     Lightning: "lightning",
-                    BaseBall: "baseball",
+                    Baseball: "baseball",
                     "Shamrock Coins": "shamrockCoins",
                     "End Of The Rainbow": "endRainbow",
                     "Easter Field": "easterField"
